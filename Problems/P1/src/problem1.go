d7ebc751ba10d24d997a0cfa234b0c8d62fb0e19
Cuken <colin.uken@gmail.com>
2020-07-26 23:26:26 -0700

    Added solution and helper function to track time taken

--- Problems/P1/src/problem1.go
+++ Problems/P1/src/problem1.go
@@ -1,19 +1,27 @@
-//This is the solution to Problem 1 for Project Euler.
+// This is the solution to Problem 1 for Project Euler.
 
 package main
 
-import "log"
+import (
+	"log"
+	"time"
 
-//Define our entry point.
+	helper "github.com/cuken/ProjectEuler/Helper"
+)
+
+// Define our entry point.
 func main() {
 
-	//Start at 0
+	// Track the time required to complete the solution
+	defer helper.TrackTime(time.Now(), "Problem 1")
+
+	// Start at 0
 	sum := 0
 
-	//Start at 1 and go through
+	// Start at 1 and go through
 	for i := 1; i < 1000; i++ {
 
-		//check if I is divisible by 3 or 5;
+		// Check if I is divisible by 3 or 5;
 		if i%3 == 0 || i%5 == 0 {
 			sum += i
 		}
